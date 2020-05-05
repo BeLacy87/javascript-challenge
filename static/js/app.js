1023c32571d1060a181441e4f4821c95c81376c4
BeLacy87 <belacy87@gmail.com>
2020-05-05 14:39:23 -0400

    filter data based on request and append to table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//select elements from index.html
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+var tbody=d3.select("tbody");
+//set up handlers
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  d3.event.preventDefault();
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  var matches = []
+  // Use the form input to filter the data by date
+  tableData.forEach(event => {
+    Object.values(event).forEach(value =>{
+      if (inputValue === value ){
+        matches.push(event);      
+                    }
+                }
+            )
+        }
+        
+    )
+    console.log(matches); 
+
+    matches.forEach((match) => {
+        var row = tbody.append("tr");
+        Object.entries(match).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+
+                    }  
+                )  
+            }
+        )
+    }
